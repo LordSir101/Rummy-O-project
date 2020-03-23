f680c97fc7ca46a23e6d9e4036ff796c45454482
graemsheppard <graemsheppard@gmail.com>
2020-03-23 15:05:02 -0400

    server and client setup

---
+++ Code/Client/client.js
@@ -0,0 +1,5 @@
+var socket = io();
+
+window.onload = function () {
+  console.log("client.js successfully loaded!");
+};
---
+++ Code/Server/server.js
@@ -0,0 +1,33 @@
+/*
+  SERVER COMMANDS: Navigate Command Prompt to Rummy-O/server to use these commands
+    npm run dev (This will run the developer version - server will restart when changes are made)
+    npm start (This will run the release version - server will not restart when changes are made)
+    CTRL+C to stop the server
+*/
+
+// Dependencies
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const app = express();
+const port = 3000;
+
+// Assign the working directory
+const clientPath = `${__dirname}/../client`; //Note you have to use these quotes ``
+console.log(`Serving from ${clientPath}`);
+app.use(express.static(clientPath));
+
+// Create the server
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+
+
+// Run the server
+server.on('error', (err) =>{
+  console.error('server error:' + err);
+});
+
+server.listen(port, 'localhost');
+console.log(`Rummy-O started on port: ${port}`);
