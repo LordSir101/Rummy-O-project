4ba865c478bcc726af02dcf0032541ce91419286
SohailSM <59832093+SohailSM@users.noreply.github.com>
2020-03-26 16:00:54 -0400

    Update Deck.js

--- Code/Server/Deck.js
+++ Code/Server/Deck.js
@@ -17,6 +17,8 @@ class Deck {
         this.deck.push(new Card(suit, value));
       }
     }
+    this.deck.push(new Card('Red', 'Joker'))
+    this.deck.push(new Card('Black', 'Joker'))
     return this.deck;
   }
   deal () {
@@ -41,7 +43,8 @@ class Deck {
 }
 
 const suits = ['Black', 'Red', 'Blue', 'Yellow'];
-const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 'Joker'];
+const values = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13];
+
 let deck = new Deck();
 deck.createDeck(suits, values)
 console.log(deck.shuffle());
